517fe96470de5554ace0bef3cda153f9be73dc53
ZentCashDevelopers <69402035+ZentCashDevelopers@users.noreply.github.com>
2024-11-30 10:43:40 +0100

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -345,7 +345,7 @@ namespace CryptoNote
     const uint8_t P2P_MINIMUM_VERSION = 12;
 
     // This defines the minimum P2P version required for lite blocks propogation
-    const uint8_t P2P_LITE_BLOCKS_PROPOGATION_VERSION = 4;
+    const uint8_t P2P_LITE_BLOCKS_PROPOGATION_VERSION = 200;
 
     // This defines the number of versions ahead we must see peers before we start displaying
     // warning messages that we need to upgrade our software.
